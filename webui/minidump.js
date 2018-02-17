05c8c8bbf49d3a337164c7bc7942a8abfac3297e
Ted Campbell <dev@tynopex.net>
2018-02-17 12:27:59 -0500

    Render minidump memory map

--- webui/minidump.js
+++ webui/minidump.js
@@ -41,6 +41,43 @@ class MinidumpViewer {
         reader.readAsArrayBuffer(file);
     }
 
+    render_memory(mem_info, dom) {
+        // State Flags
+        const MEM_COMMIT    = 0x00001000;
+        const MEM_RESERVE   = 0x00002000;
+        const MEM_FREE      = 0x00010000;
+
+        // Type Flags
+        const MEM_PRIVATE   = 0x00020000;
+        const MEM_MAPPED    = 0x00040000;
+        const MEM_IMAGE     = 0x01000000;
+
+        let list = document.createElement('ul');
+        for (let item of mem_info) {
+            let elem = document.createElement('li');
+            elem.append(item.BaseAddress.toString(16).padStart(12, '0'));
+
+            // Memory state sets CSS class
+            if (item.State == MEM_COMMIT) {
+                elem.className = "commit";
+            } else if (item.State == MEM_FREE) {
+                elem.className = "free";
+            } else if (item.State == MEM_RESERVE) {
+                elem.className = "reserve";
+            } else {
+                elem.className = "unknown";
+            }
+
+            // Mark regions that are part of image
+            if (item.Type & MEM_IMAGE) {
+                elem.className += " image";
+            }
+
+            list.appendChild(elem);
+        }
+        dom.appendChild(list);
+    }
+
     show_result(result) {
         this.body.innerHTML = "";
         this.body.append("Header Signature: " + result.magic);
@@ -50,6 +87,12 @@ class MinidumpViewer {
         this.body.append("Byte Sum: " + result.sum);
         this.body.append(document.createElement('br'));
         this.body.append("Data Size: " + result.bytelen);
+
+        let mem_dom = document.createElement('div');
+        mem_dom.className = "meminfo";
+        let mem_info = JSON.parse(result.memory_info);
+        this.render_memory(mem_info, mem_dom);
+        this.body.append(mem_dom);
     }
 
     onmessage(e) {
