2d67a117703acf8d09d80ccedbdb6075f0cf6abc
Ted Campbell <dev@tynopex.net>
2018-02-19 19:23:01 -0500

    Cleanup WASM calling

--- webui/minidump-worker.js
+++ webui/minidump-worker.js
@@ -10,18 +10,6 @@ class MinidumpProcessor {
         this.responder = responder;
     }
 
-    writeArrayToMemory(src, ptr) {
-        let dst = new Uint8Array(wasm.exports.memory.buffer);
-        dst.set(src, ptr);
-    }
-
-    UTF8ToString(ptr) {
-        let src = new Uint8Array(wasm.exports.memory.buffer, ptr);
-        let null_index = src.indexOf(0x00);
-        let dec = new TextDecoder('utf-8');
-        return dec.decode(src.subarray(0, null_index));
-    }
-
     get_magic(data) {
         let view = new DataView(data);
 
@@ -33,64 +21,78 @@ class MinidumpProcessor {
         return magic;
     }
 
-    wasm_memory_info(data) {
-        // Copy data into WASM
-        let view = new Uint8Array(data);
-        let wasm_data = wasm.exports.alloc_buffer(data.byteLength);
-        this.writeArrayToMemory(view, wasm_data);
+    // Allocate bytes in WASM
+    wasm_alloc(nbyte) {
+        let ptr = wasm.exports.alloc_buffer(nbyte);
+        return { ptr: ptr, len: nbyte };
+    }
 
-        // Run WASM
-        let raw = wasm.exports.minidump_memory_info(wasm_data, data.byteLength);
+    // Free a previous WASM allocation
+    wasm_free(wasm_buf) {
+        wasm.exports.free_buffer(wasm_buf.ptr, wasm_buf.len);
+        delete wasm_buf.len;
+        delete wasm_buf.ptr;
+    }
+
+    // Allocates memory in WASM and copies from an ArrayBuffer
+    data_to_wasm(arrayBuffer) {
+        let wasm_buf = this.wasm_alloc(arrayBuffer.byteLength);
+        let wasm_mem = wasm.exports.memory.buffer;
+
+        let src = new Uint8Array(arrayBuffer);
+        let dst = new Uint8Array(wasm_mem, wasm_buf.ptr);
+        dst.set(src);
+
+        return wasm_buf;
+    }
+
+    // Transfer a JSON string from WASM into JS and free WASM memory
+    wasm_to_json(wasm_ptr) {
+        let wasm_mem = wasm.exports.memory.buffer;
+
+        // Find span of null-terminated string (excluding null)
+        let src = new Uint8Array(wasm_mem, wasm_ptr);
+        let len = src.indexOf(0x00);
+        src = src.subarray(0, len);
+
+        // Decode string data
+        let decoder = new TextDecoder('utf-8');
+        let json = decoder.decode(src);
 
         // Release WASM buffer
-        wasm.exports.free_buffer(wasm_data, data.byteLength);
-        wasm_data = null;
-
-        // Extract JSON result
-        let json = this.UTF8ToString(raw);
-
-        // Release buffer
-        wasm.exports.release_json(raw);
-        raw = null;
+        wasm.exports.release_json(wasm_ptr);
 
         return json;
     }
 
-    wasm_module_info(data) {
-        // Copy data into WASM
-        let view = new Uint8Array(data);
-        let wasm_data = wasm.exports.alloc_buffer(data.byteLength);
-        this.writeArrayToMemory(view, wasm_data);
+    wasm_memory_info(wasm_buf) {
+        let raw = wasm.exports.minidump_memory_info(wasm_buf.ptr, wasm_buf.len);
+        return this.wasm_to_json(raw);
+    }
 
-        // Run WASM
-        let raw = wasm.exports.minidump_module(wasm_data, data.byteLength);
-
-        // Release WASM buffer
-        wasm.exports.free_buffer(wasm_data, data.byteLength);
-        wasm_data = null;
-
-        // Extract JSON result
-        let json = this.UTF8ToString(raw);
-
-        // Release buffer
-        wasm.exports.release_json(raw);
-        raw = null;
-
-        return json;
+    wasm_module_info(wasm_buf) {
+        let raw = wasm.exports.minidump_module(wasm_buf.ptr, wasm_buf.len);
+        return this.wasm_to_json(raw);
     }
 
     process(data) {
+        // Copy minidump to WASM memory
+        let wasm_data = this.data_to_wasm(data);
+
         let magic = this.get_magic(data);
-        let module_info = this.wasm_module_info(data);
-        let memory_info = this.wasm_memory_info(data);
+        let module_info = this.wasm_module_info(wasm_data);
+        let memory_info = this.wasm_memory_info(wasm_data);
+
+        // Release WASM memory
+        this.wasm_free(wasm_data);
 
         // Send response to caller
         this.responder.postMessage({
             'topic': 'result',
             'magic': magic,
+            'bytelen': data.byteLength,
             'module_info': module_info,
             'memory_info': memory_info,
-            'bytelen': data.byteLength,
         });
     }
 }
